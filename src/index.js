d9179b06a7565212a5073c63ee7639a74bb5a83e
Viktor <vitya.kibirev@gmail.com>
2018-08-11 20:20:12 +0300

    version 1.0

--- src/index.js
+++ src/index.js
@@ -2,45 +2,131 @@ let canv = document.querySelector('#canv');
 let ctx = canv.getContext('2d');
 let h = canv.height;
 let w = canv.width;
+let platformWidth = 70;
+let platformHeight = 8;
+let platformPosition = (w - platformWidth) / 2;
+let leftPress = false;
+let rigtPress = false;
+let brickRowCount = 7;
+let brickColumnCount = 10;
+let brickWidth = 40;
+let brickHeight = 20;
+let brickPadding = 20;
+let brickOffsetTop = 20;
+let brickOffsetLeft = 55;
 
-drawRects();
+let ball = {
+    x: w / 2, y: h - 30,
+    radius: 8, color: 'green',
+    dx: 2, dy: 2
+}
 
-function drawRects() {
-    let cols = 10;
-    let rows = 7;
-    let sw = w / cols;
-    let sh = (h / cols) / 1.5;
-    let pad = sw / 3;
+    let bricks = [];
+    for(c=0; c<brickColumnCount; c++) {
+        bricks[c] = [];
+        for(r=0; r<brickRowCount; r++) {
+            bricks[c][r] = { x: 0, y: 0, status: 1 };
+        }
+    }
 
-    for (let i = 0; i < rows; i++) {
-        for (let k = 0; k < cols; k++) {
-            let x = sw * k + pad / 2;
-            let y = sh * i + pad / 2;
-            ctx.fillRect(x, y, sw - pad, sh - pad);
+document.addEventListener('mousemove', mouseMoveHandler, false);
+
+function mouseMoveHandler(e) {
+    let relativeX = e.clientX - canv.offsetLeft;
+
+    if(relativeX > 0 && relativeX < w) {
+        platformPosition = relativeX - platformPosition/2;
+    }
+}
+
+function gameover() {
+    ctx.font = '60px Times New Roman';
+    ctx.fillStyle='#f24343';
+    ctx.strokeStyle='#d63939';
+    ctx.fillText( 'GAME OVER' , w/4, h/2);
+    ctx.strokeText( 'GAME OVER' , w/4, h/2);
+}
+
+function drawBall() {    
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPlatform() {
+    ctx.beginPath();
+    ctx.rect(platformPosition, h-platformHeight, platformWidth, platformHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status == 1) {
+                let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
 
-let ball = {
-    x: 300, y: 300,
-    radius: 10, color: 'green',
-    dx: 2, dy: 2
-}
-
-drawBall(ball);
-function drawBall(ball) {  
-    // ctx.clearRect
-    ctx.beginPath();    
-    ctx.fillStyle = ball.color;
-    ctx.moveTo(ball.x, ball.y);
-    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.closePath();
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-    if (ball.x < 500) {
-        requestAnimationFrame(
-            function() {drawBall(ball);}
-        );
+function collision() {
+    for(c=0; c<brickColumnCount; c++) {
+    for(r=0; r<brickRowCount; r++) {
+        let b = bricks[c][r];
+    if(b.status == 1) {
+    if(ball.x > b.x && ball.x < b.x+brickWidth && ball.y > b.y && ball.y < b.y+brickHeight) {
+       ball.dy = -ball.dy;
+     b.status = 0;
+                }
+            }
+        }
     }
 }
+
+function draw() {
+    ctx.clearRect(0, 0, w, h);
+    drawBall();
+    drawPlatform(); 
+    drawBricks();
+    collision();   
+
+    if(ball.x + ball.dx > w-ball.radius || ball.x + ball.dx < ball.radius) {
+        ball.dx = -ball.dx;
+    }
+
+    if(ball.y + ball.dy < ball.radius) {
+        ball.dy = -ball.dy;
+    }
+    else if(ball.y + ball.dy > h-ball.radius) {
+        if(ball.x > platformPosition && ball.x < platformPosition + platformWidth) {
+            ball.dy = -ball.dy;
+        }
+        else {            
+			gameover();            
+        }
+    }
+    
+    if(rigtPress && platformPosition < h-ball.radius) {
+        platformPosition += 7;
+    }
+    else if(leftPress && platformPosition > 0) {
+        platformPosition -= 7;
+    }
+
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+setInterval(draw, 10);
